468b0cb8b753c3bcaa568790e8e2b520aaff34af
j4w3 <github@thingymail.com>
2023-10-09 23:40:54 -0700

    Third attempt backlinking

--- gatsby-node.js
+++ gatsby-node.js
@@ -95,6 +95,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       title: String
       description: String
       date: Date @dateformat
+      link: String
     }
     
     type Fields {
